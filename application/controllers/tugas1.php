169a867cd042148525b0a20e6484faa7dff617ae
selebgrup22 <stellamaris3460@gmail.com>
2024-03-28 13:01:22 +0700

    commit pertama

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,11 @@
+<?php
+class tugas1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Halo! Perkenalkan";
+        echo "Nama saya STELA MARIS bisa di panggil STELLA
+            asal saya dari kuala mandor b. dan sekarang tinggal di ampera raya sui.ambawang
+            Saya sangat suka bermain game online dan suka dengerin musik juga";
+    }
+}
