8aa4d9a8f1d965939c716dd76d3572df50334859
selebgrup22 <stellamaris3460@gmail.com>
2024-04-24 12:35:08 +0700

    pertemuan 5 dan tugas 4

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -11,12 +11,30 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
+        $this->form_validation->set_rules('kode','kode matakuliah',
+            'required|min_length[3]', [
+            'required' => 'kode matakuliah harus diisi',
+            'min_lenght' => 'kode terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('nama','nama matakuliah',
+            'required|min_length[3]', [
+            'required' => 'nama matakuliah harus diisi',
+            'min_lenght' => 'nama matakuliah terlalu pendek'
+        ]); 
+        $this->form_validation->set_rules('sks','SKS',
+            'required', [
+            'required' => 'SKS harus diisi',
+        ]);
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+            } else {
+                $data = [
+                    'kode' => $this->input->post('kode'),
+                        'nama' => $this->input->post('nama'),
+                        'sks' => $this->input->post('sks')
+                ];
 
-        $this->load->view('view-data-matakuliah', $data);
+                $this->load->view('view-data-matakuliah', $data);
+            }
+        }
     }
-}
