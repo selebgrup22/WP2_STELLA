ac2def7aa0d1d59d06ece0784360fcf942b6f660
selebgrup22 <stellamaris3460@gmail.com>
2024-06-03 14:40:13 +0700

    pertemuan 10  tugas 8

--- application/views/user/ubah-profile.php
+++ application/views/user/ubah-profile.php
@@ -22,7 +22,7 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?= base_url('assets/img/profile/images.jpg') . $user['image']; ?>" class="img-thumbnail" alt="">
+                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
